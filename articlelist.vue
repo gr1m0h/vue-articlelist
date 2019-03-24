48c6a08b6d026be5e50df717cbe7c7ca091c05ce
grimoh <dev@grimoh.net>
2019-03-24 20:11:53 +0900

    Add articlelist.vue

---
+++ articlelist.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-for="article in articles">
+            <h2><a v-bind:href="article.path">{{article.frontmatter.title}}</a></h2>
+            <p>{{article.frontmatter.description}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+		articles() {
+            return this.$site.pages
+                .filter(article => article.path.startsWith('/articles/'))
+                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+        }
+    }
+}
+</script>
